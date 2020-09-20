14c7931bec6d31285aea8999f41560f1fe624edd
stephin007 <stephinreji123@gmail.com>
2020-09-20 21:48:54 +0530

    Initialised Express Server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app =express()
+const PORT = 5000
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is running on:", PORT)
+})
