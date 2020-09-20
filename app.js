1201d0c323b0ea80c6f4227d625447daed22cefe
stephin007 <stephinreji123@gmail.com>
2020-09-20 21:55:50 +0530

    Added MiddleWare

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app =express()
 const PORT = 5000
 
+//Custom Middleware
+const CustomMiddleware = (req,res,next)=>{
+    console.log("Middleware Executed")
+    next()
+}
+
+app.use(CustomMiddleware)
+
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
