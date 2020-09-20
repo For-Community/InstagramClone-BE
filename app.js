e22a7c410704e12d2f528d9e4302b6de5ce8d06d
stephin007 <stephinreji123@gmail.com>
2020-09-21 00:24:17 +0530

    Added User Model

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./Keys')
 
+require('./models/user')
+
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
     useUnifiedTopology:true
