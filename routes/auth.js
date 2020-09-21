5fa26b4593b68db9a985c589a587332f1b708d24
stephin007 <stephinreji123@gmail.com>
2020-09-21 16:51:05 +0530

    Added SignUp Route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
 
 router.get('/', (req,res)=>{
     res.send("Hello")
@@ -11,7 +13,29 @@ router.post('/signup',(req,res)=>{
     if(!email || !password || !name){
         return res.status(422).json({error:"Please add all the fields"})
     }
-    res.json({message:"Successfully Posted!"})
+    
+    User.findOne({email: email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"User already exists with tha email"})
+        }
+        const user = new User({
+            email,
+            password,
+            name
+        })
+
+        user.save()
+        .then(user=>{
+            res.json({message:"Saved Successfully"})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 })
 
 module.exports = router
