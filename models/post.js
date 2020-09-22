a1100f11b421348a6583886bb71640fa67aa2582
stephin007 <stephinreji123@gmail.com>
2020-09-23 00:23:01 +0530

    Added Post Model

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types //this line of code connects the posts model with the user model
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    photo:{
+        type: String,
+        default: "no photo"
+    },
+    postedBy:{
+        type: ObjectId,
+        ref: 'User'
+    }
+})
+
+mongoose.model('Post', postSchema)
