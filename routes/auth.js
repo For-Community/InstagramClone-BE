e7fd2eedf181422c5936186623840aad42dd3011
stephin007 <stephinreji123@gmail.com>
2020-09-22 23:20:41 +0530

    Added JWToken for every signed in user

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../Keys')
 
 const User = mongoose.model('User')
 
@@ -58,7 +59,9 @@ router.post('/signin',(req,res)=>{
         bcrypt.compare(password, savedUser.password)
         .then(doMatch =>{
             if(doMatch){
-                res.json({message:"Successfully Signed In"})
+                // res.json({message:"Successfully Signed In"})
+                const token = jwt.sign({_id:savedUser.id}, JWT_SECRET)
+                res.json({token})
             }
             else{
                 return res.status(422).json({error:"Invalid Email or password"})
