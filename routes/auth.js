d9967fb485393f52b9e35ff6b7fe28cf4cd6023a
stephin007 <stephinreji123@gmail.com>
2020-09-24 20:25:30 +0530

    Added user data along of the  signed in user

--- routes/auth.js
+++ routes/auth.js
@@ -66,7 +66,8 @@ router.post('/signin',(req,res)=>{
             if(doMatch){
                 // res.json({message:"Successfully Signed In"})
                 const token = jwt.sign({_id:savedUser.id}, JWT_SECRET)
-                res.json({token})
+                const {_id, name, email} = savedUser
+                res.json({token, user:{_id, name, email}})
             }
             else{
                 return res.status(422).json({error:"Invalid Email or password"})
