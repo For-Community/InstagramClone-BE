21bf0d314652ad6ace6c7618d464f6a7866fcd73
stephin007 <stephinreji123@gmail.com>
2020-10-25 11:54:00 +0530

    Added comments field in the model

--- models/post.js
+++ models/post.js
@@ -18,6 +18,13 @@ const postSchema = new mongoose.Schema({
         type: ObjectId,
         ref: 'User'
     }],
+    comment:[{
+        text: String,
+        postedBy:{
+            type: ObjectId,
+            ref: 'User'
+        }
+    }],
     postedBy:{
         type: ObjectId,
         ref: 'User'
